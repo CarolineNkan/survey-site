b185850f7c8cd22a5d8d47aa06b2103ab138ad44
CarolineNkan <145355980+CarolineNkan@users.noreply.github.com>
2025-04-03 20:40:00 -0400

    🔧 Connected Render to PlanetScale

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
+
+
 import os
 
 class Config:
-    MYSQL_HOST = "aws.connect.psdb.cloud"
-    MYSQL_USER = "7fyxv54q8whnktweubsp"
-    MYSQL_PASSWORD = "pscale_pw_8RwlB3rIKYGrjjxUpNAwleSsXfg3pM3tIUy59bxcMYk"
-    MYSQL_DB = "survey_db"
-    MYSQL_PORT = 3306
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_DB = os.getenv("MYSQL_DB")
+    MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
     MYSQL_SSL_CA = "/etc/ssl/certs/ca-certificates.crt"
